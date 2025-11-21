155b03c123035d47f8e1064a422cc3a0d0215e17
Roman <rabramovskij5@gmail.com>
2025-11-21 16:14:41 +0700

    Портирован класс Meal

---
+++ FitnessAppCsharp/Meal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class Meal
+    {
+        private string id;
+        private string dietId;
+        private string mealTime;
+        private List<FoodItem> foodItems = new List<FoodItem>();
+        private int totalCalories;
+        private Nutrition nutrition;
+
+        public void AddFoodItem(FoodItem item)
+        {
+            foodItems.Add(item);
+        }
+
+        public void CalculateTotals()
+        {
+            totalCalories = 0; // Доработать: суммировать из foodItems
+        }
+
+        public void SubstituteItem(FoodItem oldItem, FoodItem newItem)
+        {
+            // Заглушка
+        }
+    }
+}
