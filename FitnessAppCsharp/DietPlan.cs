8dee80ad2f665e229139b577bab41fbbb7a8236b
Roman <rabramovskij5@gmail.com>
2025-11-21 16:12:15 +0700

    Портирован класс DietPlan

---
+++ FitnessAppCsharp/DietPlan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class DietPlan
+    {
+        private string id;
+        private string userId;
+        private int dailyCalories;
+        private Macros macros;
+        private List<Meal> mealsPerDay = new List<Meal>();
+
+        public List<Meal> GenerateDailyMeals()
+        {
+            return new List<Meal> { new Meal() };
+        }
+
+        public void AdjustMacros(Macros newMacros)
+        {
+            macros = newMacros;
+        }
+
+        public void TrackIntake(Meal meal)
+        {
+        }
+    }
+}
