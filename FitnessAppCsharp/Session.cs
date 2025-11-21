e52cc31e24d6b112abf66781207987045ffde739
Roman <rabramovskij5@gmail.com>
2025-11-21 16:14:19 +0700

    Портирован класс Session

---
+++ FitnessAppCsharp/Session.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class Session
+    {
+        private string id;
+        private string programId;
+        private string scheduledDate;
+        private int actualDuration;
+        private int burnedCalories;
+        private List<ExerciseLog> completedExercises = new List<ExerciseLog>();
+
+        public void StartSession()
+        {
+            actualDuration = 0;
+        }
+
+        public void LogExercise(ExerciseLog exercise)
+        {
+            completedExercises.Add(exercise);
+        }
+
+        public void CompleteSession()
+        {
+            burnedCalories = completedExercises.Count * 100;
+        }
+
+        public int GetBurnedCalories()
+        {
+            return burnedCalories;
+        }
+    }
+}
