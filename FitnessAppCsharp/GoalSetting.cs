d9c9901b4cd67760e54c98861f110da117c5b192
Roman <rabramovskij5@gmail.com>
2025-11-21 16:12:44 +0700

    Портирован класс GoalSetting

---
+++ FitnessAppCsharp/GoalSetting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FitnessApp
+{
+    public class GoalSetting
+    {
+        private string id;
+        private string userId;
+        private GoalType goalType;
+        private double targetValue;
+        private string startDate;
+        private string deadline;
+        private double initialValue;
+
+        public GoalSetting() { }
+
+        public GoalSetting(string id, string userId, GoalType goalType, double targetValue, string startDate, string deadline)
+        {
+            this.id = id;
+            this.userId = userId;
+            this.goalType = goalType;
+            this.targetValue = targetValue;
+            this.startDate = startDate;
+            this.deadline = deadline;
+            this.initialValue = 0.0;
+        }
+
+        public double UpdateProgress(double currentValue)
+        {
+            if (initialValue == 0.0) return 0.0;
+            double progress = 0.0;
+            if (goalType == GoalType.WeightLoss)
+            {
+                progress = (initialValue - currentValue) / (initialValue - targetValue) * 100.0;
+            }
+            return progress;
+        }
+
+        public bool IsAchieved()
+        {
+            return false;
+        }
+
+        public void ExtendDeadline(string newDate)
+        {
+            deadline = newDate;
+        }
+
+        public void SetInitialValue(double val)
+        {
+            initialValue = val;
+        }
+    }
+}
