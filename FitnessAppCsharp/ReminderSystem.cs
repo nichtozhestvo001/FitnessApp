c041b51be3f36568f14301a43ccf41e7758ac588
Roman <rabramovskij5@gmail.com>
2025-11-21 16:15:39 +0700

    Портированы классы TrainingProgram и ReminderSystem

---
+++ FitnessAppCsharp/ReminderSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FitnessApp
+{
+    public class ReminderSystem
+    {
+        private string userId;
+        private List<Reminder> reminders = new List<Reminder>();
+        private NotificationType notificationType;
+
+        public void SetReminder(Reminder reminder)
+        {
+            reminders.Add(reminder);
+        }
+
+        public void TriggerNotifications()
+        {
+            // Заглушка
+        }
+
+        public void CancelReminder(string id)
+        {
+            reminders.RemoveAll(r => r.Id == id);
+        }
+    }
+}
---
+++ FitnessAppCsharp/TrainingProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class TrainingProgram
+    {
+        private string id;
+        private string name;
+        private int durationWeeks;
+        private int frequency;
+        private List<ExerciseType> exerciseTypes = new List<ExerciseType>();
+        private Difficulty difficulty;
+
+        public List<Session> GenerateWeeklySchedule()
+        {
+            List<Session> schedule = new List<Session>();
+            for (int i = 0; i < frequency; i++)
+            {
+                schedule.Add(new Session());
+            }
+            return schedule;
+        }
+
+        public void AdjustForUser(FitnessUser user)
+        {
+        }
+
+        public void AddExerciseType(ExerciseType newType)
+        {
+            exerciseTypes.Add(newType);
+        }
+    }
+}
