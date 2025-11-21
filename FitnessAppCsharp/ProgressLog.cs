ee90e51539a3d569ad5e3fd9699b748a192921f3
Roman <rabramovskij5@gmail.com>
2025-11-21 16:13:43 +0700

    Портирован класс ProgressLog

---
+++ FitnessAppCsharp/ProgressLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class ProgressLog
+    {
+        private string userId;
+        private List<LogEntry> entries = new List<LogEntry>();
+        private List<GoalSetting> goalsLinked = new List<GoalSetting>();
+
+        public void AddEntry(LogEntry entry)
+        {
+            entries.Add(entry);
+        }
+
+        public string GenerateReport(DateRange period)
+        {
+            return "Отчет о прогрессе за период с " + period.Start + " по " + period.End;
+        }
+
+        public void VisualizeProgress()
+        {
+            Console.WriteLine("Визуализация прогресса (заглушка для графика)");
+        }
+    }
+}
