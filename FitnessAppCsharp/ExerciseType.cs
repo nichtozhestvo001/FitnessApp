ab77d58a2192e95b0fcf5155586e44f3a2eef1ce
Roman <rabramovskij5@gmail.com>
2025-11-21 16:11:51 +0700

    Портирован класс ExerciseType.cs

---
+++ FitnessAppCsharp/ExerciseType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class ExerciseType
+    {
+        private string id;
+        private string title;
+        private Category category;
+        private HashSet<Muscle> targetMuscles = new HashSet<Muscle>();
+        private HashSet<Equipment> requiredEquipment = new HashSet<Equipment>();
+        private int baseCaloriesPerMin;
+
+        public int EstimateCalories(int duration)
+        {
+            return baseCaloriesPerMin * duration;
+        }
+
+        public List<ExerciseType> FindSimilar(Category category)
+        {
+            return new List<ExerciseType>(); 
+        }
+
+        public bool IsCompatible(FitnessUser user)
+        {
+            return user != null; 
+        }
+    }
+}
