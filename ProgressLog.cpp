f8f1837664b49c8e2686d43c0b45cca78e56412d
Roman <rabramovskij5@gmail.com>
2025-11-27 23:18:23 +0700

    Доработан ProgressLog

--- ProgressLog.cpp
+++ ProgressLog.cpp
@@ -1,17 +1,17 @@
 #include "ProgressLog.hpp"
 #include <iostream>
 
-// Добавление записи
+
 void ProgressLog::addEntry(const LogEntry& entry) {
     entries.push_back(entry);
 }
 
-// Генерация отчета
+
 std::string ProgressLog::generateReport(const DateRange& period) {
-    return "Отчет о прогрессе за период с " + period.start + " по " + period.end; // Заглушка
+    return "Отчет о прогрессе за период с " + period.start + " по " + period.end; 
 }
 
-// Визуализация прогресса
+
 void ProgressLog::visualizeProgress() {
     std::cout << "Визуализация прогресса (заглушка для графика)" << std::endl;
 }
--- ProgressLog.hpp
+++ ProgressLog.hpp
@@ -9,22 +9,17 @@
 class ProgressLog
 {
 public:
-    // Конструктор по умолчанию
     ProgressLog() = default;
-    // Деструктор
     ~ProgressLog() = default;
 
 private:
-    std::string userId; // Идентификатор пользователя
-    std::vector<LogEntry> entries; // Список записей
-    std::vector<GoalSetting> goalsLinked; // Связанные цели
+    std::string userId; 
+    std::vector<LogEntry> entries; 
+    std::vector<GoalSetting> goalsLinked; 
 
 public:
-    // Добавление записи
     void addEntry(const LogEntry& entry);
-    // Генерация отчета за период
     std::string generateReport(const DateRange& period);
-    // Визуализация прогресса
     void visualizeProgress();
 };
 
