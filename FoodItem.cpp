93c0aca137017f11c1bb1b3328761d0f59aee02a
Roman <rabramovskij5@gmail.com>
2025-11-27 23:15:37 +0700

    Доработан FoodItem

--- FoodItem.cpp
+++ FoodItem.cpp
@@ -1,13 +1,12 @@
 #include "FoodItem.hpp"
 
-// Конструктор с параметрами
+
 FoodItem::FoodItem(const std::string& id, const std::string& name, int caloriesPer100g, double proteinPer100g, double carbsPer100g, double fatsPer100g)
     : id(id), name(name), caloriesPer100g(caloriesPer100g), proteinPer100g(proteinPer100g), carbsPer100g(carbsPer100g), fatsPer100g(fatsPer100g) {}
 
-// Расчет нутриентов
 Nutrition FoodItem::getNutrition(double amountG) const {
     Nutrition n;
-    double factor = amountG / 100.0; // Коэффициент для массы
+    double factor = amountG / 100.0; 
     n.calories = static_cast<int>(caloriesPer100g * factor);
     n.protein = proteinPer100g * factor;
     n.carbs = carbsPer100g * factor;
@@ -15,7 +14,7 @@ Nutrition FoodItem::getNutrition(double amountG) const {
     return n;
 }
 
-// Проверка на аллергены
+
 bool FoodItem::isAllergenFree(const std::set<Allergen>& allergens) const {
-    return true; // Заглушка, поле аллергенов отсутствует
+    return true; 
 }
--- FoodItem.hpp
+++ FoodItem.hpp
@@ -8,22 +8,19 @@ class FoodItem
 {
 public:
     FoodItem() = default;
-    // Конструктор с параметрами
     FoodItem(const std::string& id, const std::string& name, int caloriesPer100g, double proteinPer100g, double carbsPer100g, double fatsPer100g);
     ~FoodItem() = default;
 
 private:
-    std::string id; // Идентификатор продукта
-    std::string name; // Название
-    int caloriesPer100g; // Калории на 100 г
-    double proteinPer100g; // Белки на 100 г
-    double carbsPer100g; // Углеводы на 100 г
-    double fatsPer100g; // Жиры на 100 г
+    std::string id; 
+    std::string name; 
+    int caloriesPer100g; 
+    double proteinPer100g;
+    double carbsPer100g;
+    double fatsPer100g;
 
 public:
-    // Расчет нутриентов для заданного веса
     Nutrition getNutrition(double amountG) const;
-    // Проверка на отсутствие аллергенов
     bool isAllergenFree(const std::set<Allergen>& allergens) const;
 };
 
