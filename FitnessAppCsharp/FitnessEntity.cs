1ca1c5d2fb5278b4b0299cf29b483fcfe6720847
Roman <rabramovskij5@gmail.com>
2025-12-04 21:49:06 +0700

    Модифицированы файлы FitnessUser и FitnessEntity

--- FitnessAppCsharp/FitnessEntity.cs
+++ FitnessAppCsharp/FitnessEntity.cs
@@ -1,16 +1,31 @@
-﻿public abstract class FitnessEntity
+﻿using System;
+
+namespace FitnessApp
 {
-    protected string Id { get; init; }
-    protected static int TotalEntities = 0;
-
-    protected FitnessEntity(string id)
+    public abstract class FitnessEntity
     {
-        Id = id;
-        TotalEntities++;
+        protected string Id { get; init; }
+        protected static int TotalEntities = 0;
+
+        protected FitnessEntity(string id)
+        {
+            Id = id;
+            TotalEntities++;
+        }
+
+        public abstract string GetInfo();
+        public abstract FitnessEntity Clone(); // Для клонирования
+
+        public virtual double CalculateSomething() // Виртуальный метод для демонстрации
+        {
+            return 0.0;
+        }
+
+        public void CallVirtual() // Невиртуальный вызывает виртуальный
+        {
+            Console.WriteLine("Результат виртуального: " + CalculateSomething());
+        }
+
+        public static int GetTotalEntities() => TotalEntities;
     }
-
-    public abstract string GetInfo();
-    public abstract FitnessEntity Clone();
-
-    public static int GetTotalEntities() => TotalEntities;
 }
--- FitnessAppCsharp/FitnessUser.cs
+++ FitnessAppCsharp/FitnessUser.cs
@@ -2,39 +2,38 @@
 
 namespace FitnessApp
 {
-    public class FitnessUser
+    public class FitnessUser : FitnessEntity, INotifiable, ILoggable
     {
-        private string id;
         private string username;
         private string email;
         private string birthDate;
         private Gender gender;
         private double heightM;
-        private double currentWeight; 
+        private double currentWeight;
         private ActivityLevel activityLevel;
 
         private static int totalUsers = 0;
-        public static int TotalUsers => totalUsers; 
+        public static int TotalUsers => totalUsers;
 
         public static string GetAppVersion()
         {
             return "FitnessApp v1.0 - Демонстрация статического метода";
         }
 
-        public FitnessUser()
+        public FitnessUser() : base(Guid.NewGuid().ToString()) // Вызов базового конструктора
         {
             totalUsers++;
         }
 
         public FitnessUser(string id, string username, string email, string birthDate, Gender gender, double heightM, double currentWeight, ActivityLevel activityLevel)
+            : base(id) // Вызов базового конструктора с параметром
         {
-            this.id = id; 
             this.username = username;
             this.email = email;
             this.birthDate = birthDate;
             this.gender = gender;
             this.heightM = heightM;
-            this.CurrentWeight = currentWeight; 
+            this.CurrentWeight = currentWeight;
             this.activityLevel = activityLevel;
             totalUsers++;
         }
@@ -65,7 +64,7 @@ namespace FitnessApp
 
         public void SetNewWeight(double newWeight)
         {
-            this.CurrentWeight = newWeight; 
+            this.CurrentWeight = newWeight;
         }
 
         public int GetRecommendedCalories()
@@ -74,6 +73,31 @@ namespace FitnessApp
             if (this.activityLevel == ActivityLevel.High) baseCalories += 500;
             else if (this.activityLevel == ActivityLevel.Medium) baseCalories += 250;
             return baseCalories;
+        }
+
+        public override string GetInfo()
+        {
+            return $"User ID: {Id}, Username: {username}";
+        }
+
+        public override FitnessEntity Clone()
+        {
+            return new FitnessUser(Id, username, email, birthDate, gender, heightM, currentWeight, activityLevel);
+        }
+
+        public override double CalculateSomething()
+        {
+            return base.CalculateSomething() + CurrentWeight;
+        }
+
+        public void SendNotification(string message)
+        {
+            Console.WriteLine($"Notification to {username}: {message}");
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log for {username}: {message}");
         }
     }
 }
