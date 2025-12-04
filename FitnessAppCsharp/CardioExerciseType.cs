4d99c0845a2c4c575c3e274c507778db9d65edae
Roman <rabramovskij5@gmail.com>
2025-12-04 22:00:36 +0700

    Модифицирован файл Program.cs и добавлены файлы для интерфейса

---
+++ FitnessAppCsharp/CardioExerciseType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class CardioExerciseType : ExerciseType
+    {
+        protected double distanceKm; 
+
+        public CardioExerciseType() 
+        {
+        }
+
+        public override int EstimateCalories(int duration)
+        {
+            return (int)(duration * 10 + baseCaloriesPerMin);
+        }
+    }
+}
---
+++ FitnessAppCsharp/StrengthExerciseType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp
+{
+    public class StrengthExerciseType : ExerciseType
+    {
+        public int reps;
+
+        public StrengthExerciseType() 
+        {
+            reps = 0;
+        }
+
+        public override int EstimateCalories(int duration)
+        {
+            return base.EstimateCalories(duration) + reps * 5;
+        }
+    }
+}
