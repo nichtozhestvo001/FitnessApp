215fa7a8d3210e17fa0730fadef249c25d65fb56
Roman <rabramovskij5@gmail.com>
2025-12-04 22:51:56 +0700

    Изменен класс ExerciseType

--- ExerciseType.cpp
+++ ExerciseType.cpp
@@ -1,14 +1,11 @@
 #include "ExerciseType.hpp"
 #include "FitnessUser.hpp"
-
 int ExerciseType::estimateCalories(int duration) {
-    return baseCaloriesPerMin * duration;
+	return baseCaloriesPerMin * duration;
 }
-
 std::vector<ExerciseType> ExerciseType::findSimilar(Category category) {
-    return {};
+	return {};
 }
-
 bool ExerciseType::isCompatible(const FitnessUser& user) {
-    return true; 
+	return true;
 }
--- ExerciseType.hpp
+++ ExerciseType.hpp
@@ -1,30 +1,32 @@
 #ifndef EXERCISETYPE_HPP
 #define EXERCISETYPE_HPP
-
 #include <vector>
 #include <set>
 #include "types.hpp"
-
 class FitnessUser;
-
 class ExerciseType
 {
 public:
-    ExerciseType() = default;
-    ~ExerciseType() = default;
-
+	ExerciseType() = default;
+	~ExerciseType() = default;
 private:
-    std::string id;
-    std::string title; 
-    Category category; 
-    std::set<Muscle> targetMuscles; 
-    std::set<Equipment> requiredEquipment; 
-    int baseCaloriesPerMin; 
-
+	std::string id;
+	std::string title;
+	Category category;
+	std::set<Muscle> targetMuscles;
+	std::set<Equipment> requiredEquipment;
+protected:
+	int baseCaloriesPerMin; 
 public:
-    int estimateCalories(int duration);
-    std::vector<ExerciseType> findSimilar(Category category);
-    bool isCompatible(const FitnessUser& user);
+	virtual int estimateCalories(int duration);
+	std::vector<ExerciseType> findSimilar(Category category);
+	bool isCompatible(const FitnessUser& user);
+	ExerciseType(const ExerciseType&) = default; 
+	ExerciseType deepClone() const {
+		ExerciseType clone = *this;
+		clone.targetMuscles = targetMuscles;
+		clone.requiredEquipment = requiredEquipment;
+		return clone;
+	}
 };
-
 #endif
