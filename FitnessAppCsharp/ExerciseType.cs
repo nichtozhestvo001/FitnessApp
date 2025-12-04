8d5789e44167247e62c6b80aee23249775bc4e30
Roman <rabramovskij5@gmail.com>
2025-12-04 21:53:21 +0700

    Модифицирован файл ExerciseType

--- FitnessAppCsharp/ExerciseType.cs
+++ FitnessAppCsharp/ExerciseType.cs
@@ -3,28 +3,41 @@ using System.Collections.Generic;
 
 namespace FitnessApp
 {
-    public class ExerciseType
+    public class ExerciseType : ICloneable
     {
         private string id;
         private string title;
         private Category category;
         private HashSet<Muscle> targetMuscles = new HashSet<Muscle>();
         private HashSet<Equipment> requiredEquipment = new HashSet<Equipment>();
-        private int baseCaloriesPerMin;
+        protected int baseCaloriesPerMin;
 
-        public int EstimateCalories(int duration)
+        public virtual int EstimateCalories(int duration)
         {
             return baseCaloriesPerMin * duration;
         }
 
         public List<ExerciseType> FindSimilar(Category category)
         {
-            return new List<ExerciseType>(); 
+            return new List<ExerciseType>();
         }
 
         public bool IsCompatible(FitnessUser user)
         {
-            return user != null; 
+            return user != null;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public ExerciseType DeepClone()
+        {
+            ExerciseType clone = (ExerciseType)this.MemberwiseClone();
+            clone.targetMuscles = new HashSet<Muscle>(targetMuscles);
+            clone.requiredEquipment = new HashSet<Equipment>(requiredEquipment);
+            return clone;
         }
     }
 }
