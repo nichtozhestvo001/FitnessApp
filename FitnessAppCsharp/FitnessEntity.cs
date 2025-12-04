4ac21e230b01955cdd541a2efebbad21f5e21dd5
Roman <rabramovskij5@gmail.com>
2025-12-04 21:30:38 +0700

    Добавлен абстрактный класс

---
+++ FitnessAppCsharp/FitnessEntity.cs
@@ -0,0 +1,16 @@
+﻿public abstract class FitnessEntity
+{
+    protected string Id { get; init; }
+    protected static int TotalEntities = 0;
+
+    protected FitnessEntity(string id)
+    {
+        Id = id;
+        TotalEntities++;
+    }
+
+    public abstract string GetInfo();
+    public abstract FitnessEntity Clone();
+
+    public static int GetTotalEntities() => TotalEntities;
+}
