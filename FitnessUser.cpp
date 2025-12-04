7a9748b3394d13a3c095ec7311676e2b09d0f3c5
Roman <rabramovskij5@gmail.com>
2025-12-04 22:43:16 +0700

    Изменен класс FitnessUser

--- FitnessUser.cpp
+++ FitnessUser.cpp
@@ -1,67 +1,56 @@
 #include "FitnessUser.hpp"
-#include <stdexcept> 
+#include <stdexcept>
 #include <iostream>
-
 int FitnessUser::totalUsers = 0;
-
 FitnessUser::FitnessUser(const std::string& id, const std::string& username, const std::string& email, const std::string& birthDate, Gender gender, double heightM, double currentWeight, ActivityLevel activityLevel)
-    : id(id), username(username), email(email), birthDate(birthDate), gender(gender), heightM(heightM), currentWeight(currentWeight), activityLevel(activityLevel)
+	: id(id), username(username), email(email), birthDate(birthDate), gender(gender), heightM(heightM), currentWeight(currentWeight), activityLevel(activityLevel)
 {
-    totalUsers++;
-    if (this->heightM <= 0) throw std::invalid_argument("Рост должен быть положительным"); 
+	totalUsers++;
+	if (this->heightM <= 0) throw std::invalid_argument("Рост должен быть положительным");
 }
-
-FitnessUser::FitnessUser(const FitnessUser& other)
-    : id(other.id), username(other.username), email(other.email), birthDate(other.birthDate), gender(other.gender), heightM(other.heightM), currentWeight(other.currentWeight), activityLevel(other.activityLevel)
-{
-    totalUsers++; 
-}
-
 double FitnessUser::calculateBMI() const {
-    try {
-        if (this->heightM <= 0) throw std::invalid_argument("Рост должен быть положительным");
-        return this->currentWeight / (this->heightM * this->heightM); 
-    }
-    catch (const std::invalid_argument& ex) {
-        std::cerr << "Ошибка ИМТ: " << ex.what() << std::endl;
-        return 0.0;
-    }
+	try {
+		if (this->heightM <= 0) throw std::invalid_argument("Рост должен быть положительным");
+		return this->currentWeight / (this->heightM * this->heightM);
+	}
+	catch (const std::invalid_argument& ex) {
+		std::cerr << "Ошибка ИМТ: " << ex.what() << std::endl;
+		return 0.0;
+	}
 }
-
 void FitnessUser::setNewWeight(double newWeight) {
-    if (newWeight <= 0) throw std::invalid_argument("Вес должен быть положительным");
-    this->currentWeight = newWeight;
+	if (newWeight <= 0) throw std::invalid_argument("Вес должен быть положительным");
+	this->currentWeight = newWeight;
 }
-
 int FitnessUser::getRecommendedCalories() const {
-    int base = 2000;
-    if (this->activityLevel == ActivityLevel::High) base += 500;
-    else if (this->activityLevel == ActivityLevel::Medium) base += 250;
-    return base;
+	int base = 2000;
+	if (this->activityLevel == ActivityLevel::High) base += 500;
+	else if (this->activityLevel == ActivityLevel::Medium) base += 250;
+	return base;
 }
-
 int FitnessUser::getTotalUsers() {
-    return totalUsers; 
+	return totalUsers;
 }
-
 void printPrivateInfo(const FitnessUser& user) {
-    std::cout << "Приватная информация (friend): ID = " << user.id << std::endl; 
+	std::cout << "Приватная информация (friend): ID = " << user.id << std::endl;
 }
-
 bool FitnessUser::operator==(const FitnessUser& other) const {
-    return this->id == other.id; 
+	return this->id == other.id;
 }
-
 FitnessUser& FitnessUser::operator+=(double weightDelta) {
-    this->currentWeight += weightDelta; 
-    return *this;
+	this->currentWeight += weightDelta;
+	return *this;
 }
-
 std::ostream& operator<<(std::ostream& os, const FitnessUser& user) {
-    os << "Пользователь: " << user.username; 
-    return os;
+	os << "Пользователь: " << user.username;
+	return os;
 }
-
 std::string FitnessUser::getBirthDate() const {
-    return this->birthDate;
+	return this->birthDate;
+}
+std::string FitnessUser::getInfo() const {
+	return "User ID: " + id + ", Username: " + username;
+}
+FitnessEntity* FitnessUser::clone() const {
+	return new FitnessUser(id, username, email, birthDate, gender, heightM, currentWeight, activityLevel); 
 }
--- FitnessUser.hpp
+++ FitnessUser.hpp
@@ -1,45 +1,41 @@
 #ifndef FITNESSUSER_HPP
 #define FITNESSUSER_HPP
-
 #include <string>
 #include "types.hpp"
 #include <memory>
-
+#include "FitnessEntity.hpp" 
 class DerivedUser;
-
-class FitnessUser
+class FitnessUser : public FitnessEntity
 {
 public:
-    FitnessUser() = default;
-    FitnessUser(const std::string& id, const std::string& username, const std::string& email, const std::string& birthDate, Gender gender, double heightM, double currentWeight, ActivityLevel activityLevel);
-    FitnessUser(const FitnessUser& other);
-    std::string getBirthDate() const;
-    ~FitnessUser() = default;
-
+	FitnessUser() = default;
+	FitnessUser(const std::string& id, const std::string& username, const std::string& email, const std::string& birthDate, Gender gender, double heightM, double currentWeight, ActivityLevel activityLevel);
+	FitnessUser(const FitnessUser& other) = delete; 
+	std::string getBirthDate() const;
+	~FitnessUser() override { std::cout << "FitnessUser destructor" << std::endl; }
 private:
-    std::string id;
-    std::string username;
-    std::string email;
-    std::string birthDate;
-    Gender gender;
-    double heightM;
-    double currentWeight;
-    ActivityLevel activityLevel;
-
-    static int totalUsers;
-
+	std::string id;
+	std::string username;
+	std::string email;
+	std::string birthDate;
+	Gender gender;
+	double heightM;
+	double currentWeight;
+	ActivityLevel activityLevel;
+	static int totalUsers;
+protected: 
+	double getProtectedHeight() const { return heightM; }
 public:
-    double calculateBMI() const;
-    void setNewWeight(double newWeight);
-    int getRecommendedCalories() const;
-
-    static int getTotalUsers(); 
-
-    friend void printPrivateInfo(const FitnessUser& user); 
-
-    bool operator==(const FitnessUser& other) const;
-    FitnessUser& operator+=(double weightDelta);
-    friend std::ostream& operator<<(std::ostream& os, const FitnessUser& user);
+	double calculateBMI() const;
+	void setNewWeight(double newWeight);
+	int getRecommendedCalories() const;
+	static int getTotalUsers();
+	friend void printPrivateInfo(const FitnessUser& user);
+	bool operator==(const FitnessUser& other) const;
+	FitnessUser& operator+=(double weightDelta);
+	friend std::ostream& operator<<(std::ostream& os, const FitnessUser& user);
+	std::string getInfo() const override;
+	FitnessEntity* clone() const override;
+	double calculateSomething() const override { return currentWeight; } 
 };
-
 #endif
